25873053902f0c17106f027caabc72ba80f3a7fc
hossamrizk <66959451+hossamrizk@users.noreply.github.com>
2024-02-11 22:09:27 +0200

    Update pages.py

--- pages.py
+++ pages.py
@@ -1,31 +1,12 @@
 import streamlit as st
 from streamlit_webrtc import webrtc_streamer
-from main import VideoTransformer
+import pandas as pd
 from twilio.rest import Client
+from main import VideoTransformer
 
-# Set page configuration
-st.set_page_config(
-    page_title="Fire Detection Demo",
-    page_icon="🔥",
-    layout="wide"
-)
 
-def main():
-    account_sid = st.text_input("Enter your Twilio Account SID:")
-    auth_token = st.text_input("Enter your Twilio Auth Token:", type='password')
-    to_number = st.text_input("Recipient's Number with country code")
-    threshold = st.slider("Threshold", min_value=0.0, max_value=1.0, value=0.5, step=0.1)
-
-    if account_sid and auth_token:
-        try:
-            # Create Twilio client instance
-            client = Client(account_sid, auth_token)
-            token = client.tokens.create()
-        except TwilioException as e:
-            st.error(f"An error occurred while creating Twilio client: {str(e)}")
-
-    try_page(account_sid, auth_token, to_number, threshold)
 def home_page():
+
     # Sidebar
     with st.sidebar:
         if st.sidebar.button("Contact me"):
@@ -149,32 +130,52 @@ import streamlit as st
 from streamlit_webrtc import webrtc_streamer
 from main import VideoTransformer
 
-def try_page(account_sid, auth_token, to_number, threshold):
+def try_page():
+    # Sidebar
+    with st.sidebar:
+        if st.sidebar.button("Contact me"):
+            st.sidebar.markdown(
+                """
+                **Contact:-**\n
+                *Hossam El-Dein Rizk*\n
+                *Computer Vision Engineer*\n
+                hossamrizk048@gmail.com\n
+                [linkedin](https://www.linkedin.com/in/hossamrizk10/)
+                [Github](https://github.com/hossamrizk)
+                [Kaggle](https://www.kaggle.com/hossamrizk)
+                """
+            )
+
     # Main content
     st.title("Try a free demo here!")
     st.write("**Please note: This is a free demo, which does not contain all options or features and may not have optimal accuracy.**")
     st.write("""
-    **Please note:** You must have an account on Twilio before you can try this demo.
-    If you don't have one, you can create one using this [link](https://login.twilio.com/u/signup?state=hKFo2SBDSm45NjBsdzQxZElvLXZCeVBQWUdxdE1wOFAxb0FMeaFur3VuaXZlcnNhbC1sb2dpbqN0aWTZIDNOblY0OEE4dFFxV1kzb0EzazBWYzdQbzRRSEc4b1lfo2NpZNkgTW05M1lTTDVSclpmNzdobUlKZFI3QktZYjZPOXV1cks)\n
-    After creating an account, search for the console dashboard. Then scroll down to get your Account SID and Auth Token.\n
-    Enter this information, and finally, you will be able to try the demo!
+    **Please note:** You must have an account on twilio before you can try this demo.
+    If you don`t have one, you can create one using this [link](https://login.twilio.com/u/signup?state=hKFo2SBDSm45NjBsdzQxZElvLXZCeVBQWUdxdE1wOFAxb0FMeaFur3VuaXZlcnNhbC1sb2dpbqN0aWTZIDNOblY0OEE4dFFxV1kzb0EzazBWYzdQbzRRSEc4b1lfo2NpZNkgTW05M1lTTDVSclpmNzdobUlKZFI3QktZYjZPOXV1cks)\n
+    After creating an account search for console dashboard, Then scroll down to get your Account SID and Auth Token\n
+    Enter these informations, Finaly you will be able to try the demo!
     """)
-    st.write("If you don't enter the required information, you will face issues while running the video. And the live video will be stuck if it detects fire.\n"
-             "So to avoid any problems, please enter the required information first.\n"
+    st.write("If you don`t enter the required information, you will face issues while running video. And the live video will stuck if it detect fire.\n"
+             "So to avoid any problems please enter the required information first\n"
              )
     st.write("Thanks, I hope you like it. :)")
 
+    # Input fields for Twilio credentials and threshold slider
+    account_sid = st.text_input("Enter your Twilio Account SID:")
+    auth_token = st.text_input("Enter your Twilio Auth Token:", type='password')
+    to_number = st.text_input("Recipient's Number with country code")
+    threshold = st.slider("Threshold", min_value=0.0, max_value=1.0, value=0.5, step=0.1)
+
     # Create instance of VideoTransformer
     video_transformer = VideoTransformer(threshold=threshold, account_sid=account_sid, auth_token=auth_token, to_number=to_number)
-    
-    # Display the video stream and fire detection
-    if account_sid and auth_token:
-        webrtc_ctx = webrtc_streamer(key="example", video_processor_factory=lambda: video_transformer, rtc_configuration={
-            "iceServers": token.ice_servers
-        })
 
-        if not webrtc_ctx.video_transformer:
-            st.warning("Please allow access to your camera.")
+    # Display the video stream and fire detection
+    webrtc_ctx = webrtc_streamer(key="example", video_processor_factory=lambda: video_transformer)
+
+    if not webrtc_ctx.video_transformer:
+        st.warning("Please allow access to your camera.")
+
+
 
 def feedback_page():
     # Sidebar
@@ -197,5 +198,3 @@ def feedback_page():
     st.write("Your feedback is crucial in helping me enhance the effectiveness and usability of my fire detection system.")
     st.write(
         "Thank you for visiting the Fire Detection and Alert System platform. Together, let's work towards a safer, fire-free future for all.")
-if __name__ == "__main__":
-    main()
